f2395aa34a27cc45435f6327f17e49bc5ae2883f
unknown <pk9910765616@gmail.com>
2021-08-17 15:07:02 +0530

    shop section co,pletly done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import About from './components/About';
 import Header from './components/Header';
+import Menu from './components/Menu';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <div >
       <Header/>
       <About/>
+      <Shop/>
+      <Menu/>
     </div>
   );
 }
