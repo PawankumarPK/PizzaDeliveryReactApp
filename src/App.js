e966fe6f8aedf34fab72663bc1d071cf8c501679
unknown <pk9910765616@gmail.com>
2021-08-17 15:37:57 +0530

    price section done with css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Clients from './components/Clients';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Prices from './components/Prices';
 import Shop from './components/Shop';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Shop/>
       <Menu/>
       <Clients/>
+      <Prices/>
     </div>
   );
 }
