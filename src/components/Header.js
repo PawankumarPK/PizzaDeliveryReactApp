4d87254440a99c713c3d9bfd81d0c8b897889911
unknown <pk9910765616@gmail.com>
2021-08-17 12:04:41 +0530

    header part done with bg img and content

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className="banner">
+            <div className="banner__content">
+                <div className="container">
+                    <div className="banner__text">
+                        <h3>Pizza Delivery</h3>
+                        <h1>MAESTRO PIZINNI</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+                            minus ut mollitia error molestiae quia.
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
